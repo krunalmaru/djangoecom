f79369cf17aa09e258b0cfb4dd8651c04dbeaed4
krunal <krunal.maru1106@gmail.com>
2022-11-04 16:23:00 +0530

    model

---
+++ ecomm/base/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class BaseModel(models.Model):
+    uuid = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
